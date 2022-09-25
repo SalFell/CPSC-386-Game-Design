d152ebbbc9209aee7b00e40699553dd574222336
Salvador Felipe <90482212+SalFell@users.noreply.github.com>
2022-09-25 16:00:07 -0700

    Assignment 1

---
+++ hello.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# Salvador Felipe
+# CPSC 386-01
+# 2022-02-06
+# sfel@csu.fullerton.edu
+# @SalFell
+#
+# Lab 00-00
+#
+# This is a test program and it print out "Hello World!"
+#
+
+"""Above is the Header. Bellow is the main code"""
+
+
+def main():
+    """This is the main function"""
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    main()
